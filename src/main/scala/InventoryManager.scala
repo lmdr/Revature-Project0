53c977d10a37b8f6a27da93a91425af3a30e177b
Louis Duller <louis.mitchell.duller@gmail.com>
2021-10-24 19:29:38 -0400

    Implemented show_inventory()

--- src/main/scala/InventoryManager.scala
+++ src/main/scala/InventoryManager.scala
@@ -1,6 +1,35 @@
+import java.sql.SQLException
+
 object InventoryManager {
   def show_inventory(): Unit = {
     println("show_inventory()")
+    try {
+      val sql = "SELECT * FROM inventory"
+      val sql_statement = DBConnection.getConnection().createStatement()
+      val sql_results = sql_statement.executeQuery(sql)
+
+      if (!sql_results.next()) {
+        println("Error: No Results")
+      } else {
+        do {
+          val inventory_id = sql_results.getString("inventory_id")
+          val product_id = sql_results.getString("product_id")
+          val location_id = sql_results.getString("location_id")
+          val quantity = sql_results.getString("quantity")
+
+          println("{")
+          println("\tinventory_id: " + inventory_id)
+          println("\tproduct_id: " + product_id)
+          println("\tlocation_id: " + location_id)
+          println("\tquantity: " + quantity)
+          println("}\n")
+        } while (sql_results.next())
+      }
+    } catch {
+      case sql: SQLException =>
+        sql.printStackTrace()
+        println("Error: SQL")
+    }
   }
 
   def adjust(): Unit = {
